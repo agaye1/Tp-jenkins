2d376ac7e983387e69545bbf617267a23235aecb
agaye <aligaye1@gmail.com>
2017-04-19 09:48:09 +0200

    test

--- tp-jenkins/Program.cs
+++ tp-jenkins/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 [assembly: CLSCompliant(true)]
+
 namespace TpJenkins
 {
     public static class Program
@@ -11,13 +13,14 @@ namespace TpJenkins
         static void Main()
         {
             Console.WriteLine("Hello world");
+
             Console.ReadLine();
         }
         public static int  Add(int a, int b)
         {
             return a + b;
         }
-        public static  int Division (int a, int b)
+        public static  int Division(int a, int b)
         {
             return a / b;
         }
